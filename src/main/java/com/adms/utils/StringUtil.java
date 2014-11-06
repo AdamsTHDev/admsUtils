4ba3c668d9bf2f5827797a3f04529af86313ef2d
kampon8 <kampon8@gmail.com>
2014-11-06 19:30:12 +0700

    Added method StringUtil.removeDoubleSpace(String s)

--- src/main/java/com/adms/utils/StringUtil.java
+++ src/main/java/com/adms/utils/StringUtil.java
@@ -1,6 +1,11 @@
 package com.adms.utils;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class StringUtil {
 
-	
+	public static String removeDoubleSpace(String s)
+	{
+		return s == null ? null : StringUtils.replace(s, "  ", " ");
+	}
 }
