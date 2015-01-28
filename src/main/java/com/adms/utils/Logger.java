d95e0729193e2c7c1079203ad799253b4866f640
kampon.pan <kampon.pan@AEGONCLIENT34.AEGONTH.COM>
2015-01-28 10:07:57 +0700

    modified log message format

--- src/main/java/com/adms/utils/Logger.java
+++ src/main/java/com/adms/utils/Logger.java
@@ -5,6 +5,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Logger {
 
 	private static final String LOG_TIME_FORMAT = "yyyyMMdd HH:mm:ss:SSS";
@@ -16,6 +18,8 @@ public class Logger {
 	public static final int WARN = 30;
 	public static final int ERROR = 20;
 	public static final int FATAL = 10;
+
+	public static final int STACK_TRACE_LEVEL = 5;
 
 	private int logLevel = 40;
 
@@ -44,9 +48,31 @@ public class Logger {
 		this.logLevel = logLevel;
 	}
 
+	private String getClassName()
+	{
+		return Thread.currentThread().getStackTrace()[STACK_TRACE_LEVEL].getClassName();
+	}
+
+	private String getMethodName()
+	{
+		return Thread.currentThread().getStackTrace()[STACK_TRACE_LEVEL].getMethodName();
+	}
+
+	private int getLineNumber()
+	{
+		return Thread.currentThread().getStackTrace()[STACK_TRACE_LEVEL].getLineNumber();
+	}
+
 	private String buildMessage(String prefix, String message)
 	{
-		return new StringBuilder("").append(prefix).append(this.logDf.format(Calendar.getInstance(LOG_TIME_LOCALE).getTime())).append(" ").append(message).toString();
+		String className = getClassName();
+		className = className.substring(className.lastIndexOf('.') + 1);
+		String methodName = getMethodName();
+		int lineNumber = getLineNumber();
+
+		String classInfo = StringUtils.leftPad(className + "." + methodName, 50, ' ') + ":" + StringUtils.rightPad("" + lineNumber, 4);
+
+		return new StringBuilder("").append(prefix).append(this.logDf.format(Calendar.getInstance(LOG_TIME_LOCALE).getTime())).append(" ").append(classInfo).append(" - ").append(message).toString();
 	}
 
 	private void log(String prefix, String message)
